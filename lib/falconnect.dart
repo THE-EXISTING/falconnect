6de3e6f6d0980c1b51273cc266545239700564d0
Nonthawit Doungsodsri <nonthawit.kub@gmail.com>
2023-07-05 20:31:37 +0700

    [+] update

--- lib/falconnect.dart
+++ lib/falconnect.dart
@@ -1,5 +1,6 @@
 library falconnect;
 
+export 'package:connectivity_plus/connectivity_plus.dart';
 export 'package:web_socket_channel/web_socket_channel.dart';
 export 'package:dio/dio.dart';
 export 'package:dio/io.dart';
--- lib/lib.dart
+++ lib/lib.dart
@@ -5,8 +5,8 @@ export 'package:ansicolor/ansicolor.dart';
 export 'package:falconnect/utils/nlog.dart';
 export 'package:falmodel/falmodel.dart';
 export 'package:faltool/faltool.dart';
-export 'package:connectivity_plus/connectivity_plus.dart';
 
+export 'package:connectivity_plus/connectivity_plus.dart';
 export 'package:web_socket_channel/web_socket_channel.dart';
 export 'package:dio/dio.dart';
 export 'package:dio/io.dart';
