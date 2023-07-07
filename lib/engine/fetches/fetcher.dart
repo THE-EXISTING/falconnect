dba7d5ae83224cab2fcbec5baf5eeba8f2128ff5
Nonthawit Doungsodsri <nonthawit.kub@gmail.com>
2023-07-07 15:49:26 +0700

    [+] update

--- lib/engine/fetches/fetcher.dart
+++ lib/engine/fetches/fetcher.dart
@@ -1,42 +1,71 @@
 import 'package:falconnect/lib.dart';
 
 class EitherFetcher<T> {
+  EitherFetcher([StreamController<WidgetState<T?>>? controller])
+      : _streamController = controller ?? StreamController<WidgetState<T?>>();
+
   StreamSubscriptionWrapper? subscription;
 
-  bool get isClose => subscription?.isClosed ?? true;
+  // bool get isClose => subscription?.isClosed ?? true;
+  bool get isClose => _streamController.isClosed;
 
-  Future<void> fetch(
-    Stream<Either<Failure, T>> call, {
-    required Function(WidgetState<T?> data) onFetch,
-    Function(Failure failure)? onFail,
-  }) async {
-    onFetch(WidgetState(WidgetStatus.loading, data: null));
+  final StreamController<WidgetState<T?>> _streamController;
 
-    if (subscription != null) {
-      subscription?.cancel();
-    }
-    subscription = StreamSubscriptionWrapper<Either<Failure, T>>(
-      call,
-      onData: (Either<Failure, T> data) {
+  Stream<WidgetState<T?>> get stream => _streamController.stream;
+
+  Stream<WidgetState<T?>> fetch(
+    Stream<Either<Failure, T>> call,
+    // {
+    // required Function(WidgetState<T?> data) onFetch,
+    // Function(Failure failure)? onFail,
+    // }
+  ) {
+    // onFetch(WidgetState(WidgetStatus.loading, data: null));
+    _streamController.add(WidgetState(WidgetStatus.loading, data: null));
+    _streamController.addStream(call.transform(
+        StreamTransformer<Either<Failure, T>, WidgetState<T>>.fromHandlers(
+      handleData: (Either<Failure, T> data, EventSink<WidgetState<T>> sink) {
         data.fold(
-          (Failure fail) => onFail?.call(fail),
-          (T data) => onFetch(WidgetState(WidgetStatus.success, data: data)),
+          (failure) => throw failure,
+          (T data) => sink.add(WidgetState(WidgetStatus.success, data: data)),
         );
       },
-      onError: (error, stacktrace) {
-        if (onFail == null) Log.e(error, stacktrace);
-        onFail?.call(Failure.fromException(
-          exception: error,
-          stacktrace: stacktrace,
-        ));
-      },
-      onDone: () {
+      handleDone: (sink) {
         Log.success('Fetch onDone');
+        _streamController.close();
       },
-    );
+      handleError: (error, stackTrace, sink) {
+        throw Failure.fromException(exception: error, stacktrace: stackTrace);
+      },
+    )));
+    return _streamController.stream;
+
+    // if (subscription != null) {
+    //   subscription?.cancel();
+    // }
+    // subscription = StreamSubscriptionWrapper<Either<Failure, T>>(
+    //   call,
+    //   onData: (Either<Failure, T> data) {
+    //     data.fold(
+    //       (Failure fail) => onFail?.call(fail),
+    //       (T data) => onFetch(WidgetState(WidgetStatus.success, data: data)),
+    //     );
+    //   },
+    //   onError: (error, stacktrace) {
+    //     if (onFail == null) Log.e(error, stacktrace);
+    //     onFail?.call(Failure.fromException(
+    //       exception: error,
+    //       stacktrace: stacktrace,
+    //     ));
+    //   },
+    //   onDone: () {
+    //     Log.success('Fetch onDone');
+    //   },
+    // );
   }
 
   void close() {
+    _streamController.close();
     subscription?.cancel();
   }
 }
--- lib/engine/fetches/fetcher_list.dart
+++ lib/engine/fetches/fetcher_list.dart
@@ -3,54 +3,68 @@ import 'package:falconnect/lib.dart';
 class FetcherList {
   final Map<dynamic, EitherFetcher> _fetcherMap = {};
 
-  Future<void> fetchStream<T>({
+  Stream<WidgetState<T?>> fetchStream<T>({
     required Object key,
     required Stream<Either<Failure, T>> call,
-    required Function(WidgetState<T?> data) onFetch,
-    Function(Failure failure)? onFail,
+    // required Function(WidgetState<T?> data) onFetch,
+    // Function(Failure failure)? onFail,
     bool debounceFetch = true,
-  }) async {
-    _removeFetcherIfClose();
-
-    if (!debounceFetch) await _forceCloseFetcherByKey(key);
-
-    EitherFetcher<T>? fetcher = _fetcherMap[key] as EitherFetcher<T>?;
-
-    if (fetcher != null && debounceFetch) return;
-    if (fetcher == null) {
-      fetcher = EitherFetcher<T>();
+  }) {
+    if (_canFetch(key, debounceFetch)) {
+      final fetcher = EitherFetcher<T>();
       _fetcherMap[key] = fetcher;
+      return fetcher.fetch(call);
+    } else {
+      Log.w('Debounce fetch use same stream !!!');
+      return (_fetcherMap[key]! as EitherFetcher<T>).stream;
     }
 
-    return fetcher.fetch(
-      call,
-      onFetch: onFetch,
-      onFail: onFail,
-    );
+    // if (!debounceFetch) _forceCloseFetcherByKey(key);
+    //
+    // EitherFetcher<T>? fetcher = _fetcherMap[key] as EitherFetcher<T>?;
+    //
+    // if (fetcher != null && debounceFetch) return;
+    // if (fetcher == null) {
+    //   fetcher = EitherFetcher<T>();
+    //   _fetcherMap[key] = fetcher;
+    // }
+    //
+    // return fetcher.fetch(
+    //   call,
+    //   // onFetch: onFetch,
+    //   // onFail: onFail,
+    // );
   }
 
-  Future<void> fetchFuture<T>({
+  Stream<WidgetState<T?>> fetchFuture<T>({
     required Object key,
     required Future<Either<Failure, T>> call,
-    required Function(WidgetState<T?> data) onFetch,
-    Function(Failure failure)? onFail,
+    // required Function(WidgetState<T?> data) onFetch,
+    // Function(Failure failure)? onFail,
     bool debounceFetch = true,
   }) =>
       fetchStream<T>(
         key: key,
         call: Stream.fromFuture(call),
-        onFetch: onFetch,
-        onFail: onFail,
+        // onFetch: onFetch,
+        // onFail: onFail,
         debounceFetch: debounceFetch,
       );
 
-  Future<void> _forceCloseFetcherByKey(dynamic key) async {
+  void _forceCloseFetcherByKey(dynamic key) {
     final fetcher = _fetcherMap[key];
     fetcher?.close();
   }
 
-  void _removeFetcherIfClose() {
+  bool _canFetch(Object key, bool debounceFetch) {
     _fetcherMap.removeWhere((key, value) => value.isClose);
+    if (debounceFetch) {
+      return _fetcherMap[key] == null;
+    } else {
+      final fetcher = _fetcherMap.remove(key);
+      fetcher?.close();
+      return true;
+    }
   }
 
   void close() {
